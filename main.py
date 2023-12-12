6e1b167171b0ccc598842e728607542d9c488934
omka <mcscare12@gmail.com>
2023-12-12 21:08:54 +0300

    Make main.py more informative

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from aionumbers import get_phone_numbers_from_urls
+import asyncio
+
+from aionumbers import get_phone_numbers_from_url_aio, get_phone_numbers_from_urls_aio, get_phone_numbers_from_urls
+
+res_1 = asyncio.run(get_phone_numbers_from_url_aio("https://hands.ru/company/about/", False))
+print(res_1, sep='\n', end='\n' + '-' * 50 + '\n')
 
 links = ["https://repetitors.info/",
-         "https://hands.ru/company/about/",
          "https://www.kb123.ru/",
          "https://www.mirea.ru/", ]
 
-numbers = get_phone_numbers_from_urls(links, auto=True)
-print(*numbers, sep='\n')
+res_2 = asyncio.run(get_phone_numbers_from_urls_aio(links, auto=True))
+print(*res_2, sep='\n', end='\n' + '-' * 50 + '\n')
+
+res_3 = get_phone_numbers_from_urls(links, auto=False)
+print(*res_3, sep='\n', end='\n' + '-' * 50 + '\n')
