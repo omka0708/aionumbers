ceb1aadf12b40813b56a9088023c3ff652fb82e5
omka <mcscare12@gmail.com>
2024-02-06 13:37:18 +0300

    Remove comments, add documentation

--- aionumbers/aionumbers.py
+++ aionumbers/aionumbers.py
@@ -11,15 +11,24 @@ REGEX_LOOKAHEAD = r"(?=[\W])"
 PATTERN = REGEX_LOOKBEHIND + REGEX_PHONE_CODES + REGEX_MAIN_NUMBERS + REGEX_LOOKAHEAD
 
 
-# Function for getting html plain text from url
 async def get_html(url: str) -> str:
+    """
+    Function for getting html plain text from url
+    :param url: Line with link to HTML document.
+    :return: HTML document.
+    """
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             return await response.text()
 
 
-# Function for getting numbers from one url address
-async def get_phone_numbers_from_url_aio(url: str, auto: bool = False) -> dict[str, tuple]:
+async def get_phone_numbers_from_url_aio(url: str, auto: bool = True) -> dict[str, tuple]:
+    """
+    Function for getting numbers from one url address
+    :param url: Line with link to HTML document.
+    :param auto: Auto-add Moscow city code.
+    :return: URL with parsed numbers.
+    """
     html = await get_html(url)
     phone_numbers = set()
     for number in re.finditer(PATTERN, html):
@@ -37,16 +46,24 @@ async def get_phone_numbers_from_url_aio(url: str, auto: bool = False) -> dict[s
     return {url: tuple(phone_numbers)}
 
 
-# Function for getting numbers from some url addresses
 async def get_phone_numbers_from_urls_aio(urls: list[str], auto: bool = False):
+    """
+    Function for getting numbers from some url addresses.
+    :param urls: List of links to HTML documents.
+    :param auto: Auto-add Moscow city code.
+    """
     tasks = []
     for url in urls:
         tasks.append(get_phone_numbers_from_url_aio(url, auto))
     return await asyncio.gather(*tasks)
 
 
-# Synchronous wrapper for asynchronous function
 def get_phone_numbers_from_urls(urls: list[str], auto: bool = False):
+    """
+    Synchronous wrapper for asynchronous function.
+    :param urls: List of links to HTML documents.
+    :param auto: Auto-add Moscow city code.
+    """
     return asyncio.run(get_phone_numbers_from_urls_aio(urls, auto))
 
 
